3982602ee6418b1c0c11e4b7313cc14309a95dca
Kelvin Anderson <kelvinbusiness10@gmail.com>
2023-07-29 21:50:01 +0700

    add aos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import './App.css'
 import Sejarah from './Components/Sejarah'
 import Beranda from './Components/Beranda'
 import Navbar from './Components/Navbar'
 
 function App() {
-
+  useEffect(() => {
+    AOS.init({once: true});
+    AOS.refresh();
+}, []);
   return (
     <div className='App'>
       <Navbar/>
--- src/Components/Sejarah.jsx
+++ src/Components/Sejarah.jsx
@@ -1,17 +1,18 @@
-import React from "react";
 const Sejarah = () => {
+
+
     return (
         <section id="sejarah" className="sejarah">
             <div className="sekilas-container">
                 <div className="sekilas-left">
-                    <p>Sekilas</p>
-                    <p>
+                    <p data-aos="fade-right">Sekilas</p>
+                    <p data-aos="fade-right" data-aos-delay="200">
                         "PT Sungadiman Makmur Sentosa merupakan perusahaan yang
                         berpengalaman selama belasan tahun dalam bidang distrubusi alat
                         kesehatan sebagai distributor resmi berbagai brand alat kesehatan
                         dan unggul dalam kualitas jasa pelayanan. "
                     </p>
-                    <p>
+                    <p data-aos="fade-right" data-aos-delay="400">
                         PT Sungadiman Makmur Sentosa berpergang teguh pada prinsip usaha
                         peningkatan kesehatan masyarakat melaui jasa pelayanan dengan
                         menjunjung tinggi kualitas produk yang di jual dan menjalin
@@ -19,13 +20,13 @@ const Sejarah = () => {
                         yang memiliki reputasi dikancah internasional.
                     </p>
                 </div>
-                <div className="sekilas-right">
+                <div data-aos="fade-left" data-aos-duration="2000" data-aos-delay="300" className="sekilas-right">
                     <img src="/sekilas-dokter.png"></img>
                 </div>
             </div>
             <div className="sejarah-container">
-                <p>Sejarah PT. Sungadiman Makmur Sentosa</p>
-                <div className="timeline">
+                <p data-aos="fade-up">Sejarah PT. Sungadiman Makmur Sentosa</p>
+                <div data-aos="zoom-in" className="timeline">
                     <div className="timeline-line"></div>
                     <div className="timeline-dot-container">
                             <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 26 26" fill="none">
@@ -47,19 +48,19 @@ const Sejarah = () => {
                     </div>
                 </div>
                 <div className="timeline-card-container">
-                    <div className="timeline-card">
+                    <div className="timeline-card" data-aos="fade-right" data-aos-delay="200">
                         <p>2008</p>
                         <p>Berawal dari toko retail alat kesehatan yang dijalankan dengan partner bisnis.</p>
                     </div>
-                    <div className="timeline-card">
+                    <div className="timeline-card" data-aos="fade-right" data-aos-delay="400">
                         <p>2010</p>
                         <p>Memutuskan untuk menjadi toko retail alat kesehatan yang mandiri.</p>
                     </div>
-                    <div className="timeline-card">
+                    <div className="timeline-card" data-aos="fade-right" data-aos-delay="600">
                         <p>2016</p>
                         <p>Beralih menjadi distributor alat kesehatan.</p>
                     </div>
-                    <div className="timeline-card">
+                    <div className="timeline-card" data-aos="fade-right" data-aos-delay="800">
                         <p>2022</p>
                         <p>Menjadi distributor dengan standar Sertifikasi CDAKB.</p>
                     </div>
