a403f8c2a5432802a6565a2d6b38c8898d775796
Kelvin Anderson <kelvinbusiness10@gmail.com>
2023-07-30 21:05:48 +0700

    add scroll progress bar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,35 @@
+import { useEffect, useState } from "react";
+
 const Navbar = () => {
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    const [scrollPercentage, setScrollPercentage] = useState(0);
+
+    const handleScroll = () => {
+        const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+        const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        const scrolled = (winScroll / height) * 100;
+        setScrollPercentage(scrolled);
+    };
     return (
         <nav>
-            <div className="nav-logo-container"></div>
-            <div className="navbar-links-container">
-                <a href="#beranda">Beranda</a>
-                <a href="#sejarah">Sejarah</a>
-                <a href="#misi-visi">Misi & Visi</a>
-                <a href="#nilai-perusahaan">Nilai Perusahaan</a>
-                <a href="#partner">Partner</a>
+            <div className="nav-container">
+                <div className="nav-logo-container"></div>
+                <div className="navbar-links-container">
+                    <a href="#beranda">Beranda</a>
+                    <a href="#sejarah">Sejarah</a>
+                    <a href="#misi-visi">Misi & Visi</a>
+                    <a href="#nilai-perusahaan">Nilai Perusahaan</a>
+                    <a href="#partner">Partner</a>
+                </div>
+            </div>
+            <div className="progress-container">
+                <div className="progress-bar" style={{ width: `${scrollPercentage}%` }}></div>
             </div>
         </nav>
     );
