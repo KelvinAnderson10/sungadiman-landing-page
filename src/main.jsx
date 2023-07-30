267209abfc6e960598e926991b94795e59e3b6ab
Kelvin Anderson <kelvinbusiness10@gmail.com>
2023-07-30 14:40:23 +0700

    use react-snap to increase seo

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const Main = (
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />
+</React.StrictMode>
 )
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  ReactDOM.createRoot(rootElement).hydrate(Main);
+} else {
+  ReactDOM.createRoot(rootElement).render(Main);
+}
