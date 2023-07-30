22905cbff468390fb246e34a8928fa7fab06c871
Kelvin Anderson <kelvinbusiness10@gmail.com>
2023-07-30 12:44:37 +0700

    remove unused code

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,21 +1,11 @@
-import { useState } from "react";
-
 const Navbar = () => {
-    const [activeLink, setActiveLink] = useState("beranda");
-
-    // Function to set the active link
-    const handleSetActiveLink = (link) => {
-        setActiveLink(link);
-    };
-
     return (
         <nav>
             <div className="nav-logo-container"></div>
             <div className="navbar-links-container">
                 <a href="#beranda">Beranda</a>
                 <a href="#sejarah">Sejarah</a>
-                <a href="#misi">Misi</a>
-                <a href="#visi">Visi</a>
+                <a href="#misi">Misi & Visi</a>
                 <a href="#nilai-perusahaan">Nilai Perusahaan</a>
                 <a href="#partner">Partner</a>
             </div>
